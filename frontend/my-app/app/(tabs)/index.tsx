5b5f68f2955acfdfff552260e57545b81e6468b0
yawen xue <124210098+yawenx2004@users.noreply.github.com>
2025-08-21 16:26:09 -0400

    added requirements.txt

--- frontend/my-app/app/(tabs)/index.tsx
+++ frontend/my-app/app/(tabs)/index.tsx
@@ -1,11 +1,43 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, TextInput, View } from "react-native";
 import styles from '../styles';
 
 export default function Index() {
+  const [date, setDate] = useState("");
+  const [reason, setReason] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const submitSpending = async() => {
+    try {
+      
+    } catch (error) {
+
+    }
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.section}>
-        <Text>This is where you enter spending or income.</Text>
+        <Text>Date:</Text>
+        <TextInput
+          value={date}
+          onChangeText={setDate}
+          placeholder="MM-DD-YYYY"
+        />
+
+        <Text>Reason:</Text>
+        <TextInput
+          value={reason}
+          onChangeText={setReason}
+        />
+
+        <Text>Amount:</Text>
+        <TextInput
+          value={amount}
+          onChangeText={setAmount}
+          placeholder="0.00"
+          keyboardType="numeric"
+        />
       </View>
 
       <View style={styles.section}>
