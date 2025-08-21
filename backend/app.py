d6f32316e3232d50a8593ca3434e12fe45c091de
yawen xue <124210098+yawenx2004@users.noreply.github.com>
2025-08-21 16:18:02 -0400

    got backend mongodb server up

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 import time
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+# connect to mongodb
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://yawenx:pass@budget-tracker.ibanosj.mongodb.net/?retryWrites=true&w=majority&appName=budget-tracker")
+client = MongoClient(MONGO_URI)
+spending_db = client["spending_db"]
+spending_collection = spending_db["spendings"]
+
+# TODO income collection too
+
 # register routes
-## test route
+## real routes
+app.spending_collection = spending_collection
+from routes.spending_routes import spending_bp
+app.register_blueprint(spending_bp)
+
+## test routes
 @app.route('/')
 def get_home():
     welcome_msg = "hello!! welcome to our flask frontend <3"
---
+++ backend/routes/spending_routes.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, request, current_app
+
+spending_bp = Blueprint("spending", __name__, url_prefix="/spending")
+
+@spending_bp.route("", methods=["post"])
+def add_spending():
+    data = request.get_json()
+
+    # check for required fields
+    date = data.get("data")
+    reason = data.get("reason")
+    amount = data.get("amount")
+    if not date or not reason or not amount:
+        return {"error": "missing required fields"}
+    
+    # model
+    spending_doc = {
+        "date": date,
+        "reason": reason,
+        "amount": amount,
+        "category": data.get("category"), 
+        "satisfaction": data.get("satisfaction")
+    }
+
+    # access mongo collection
+    result = current_app.spending_collection.insert_one(spending_doc)
+    return {"inserted_id": str(result.inserted_id)}
+
+@spending_bp.route("", methods=["GET"])
+def list_spendings():
+    spendings = list(current_app.spending_collection.find({}, {"_id": 0}))
+    return {"spendings": spendings}
